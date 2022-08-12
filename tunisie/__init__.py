81f778fdfb5c6180109570ea74fbada5c05a8e56
python-cinnamon <au85265@gmail.com>
2022-08-12 15:31:56 +0500

    update

--- tunisie/__init__.py
+++
@@ -1,1 +0,0 @@
-from .announce import get
--- tunisie/main.py
+++ tunisie/main.py
@@ -1,8 +1,6 @@
 
-
 import announce
 
-
-# you can pass all other arguments as well.
-announce.get(Country='Tunisie',Gouvernorat='Beja',price_min=500,price_max=600,save=True,with_photos=True)
+# you can pass all other arguments as well. 
+announce.get(Country='Tunisie',Gouvernorat='Beja',price_min=500,price_max=600,save=True,Type='Appart. 1',with_photos=True)
 
