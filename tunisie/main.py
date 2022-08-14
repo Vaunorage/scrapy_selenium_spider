606755bc1029d312b7e9cfa669de55c12324ac85
EhsanUllah <88642086+Ehsan-U@users.noreply.github.com>
2022-08-14 07:18:13 +0500

    Update main.py

--- tunisie/main.py
+++ tunisie/main.py
@@ -2,5 +2,6 @@
 import announce
 
 # you can pass all other arguments as well. 
+# pass recurrence 60 to run it every hour
 announce.get(Country='Tunisie',Gouvernorat='Beja',price_min=500, price_max=600,save=False,Type='Appart. 1',with_photos=True,recurrence=1)
 
